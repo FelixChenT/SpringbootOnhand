407f5af179e9f10b4721cfe07520948f9b91ed04
FelixChen <felixchentao@163.com>
2024-08-19 23:57:58 +0800

    update

---
+++ src/main/java/com/chentao/demo01/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.chentao.demo01.handler;
+
+import com.chentao.demo01.util.JsonResultUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+    // 打印log
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    // ……
+
+    /**
+     * 缺少请求参数异常
+     * @param ex HttpMessageNotReadableException
+     * @return
+     */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public JsonResultUtils handleHttpMessageNotReadableException(
+            MissingServletRequestParameterException ex) {
+        logger.error("缺少请求参数，{}", ex.getMessage());
+        return new JsonResultUtils("400", "缺少必要的请求参数");
+    }
+}
